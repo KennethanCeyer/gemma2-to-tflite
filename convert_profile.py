cc015c63c0031ba8e27458937f6c4322a14d8881
KennethanCeyer <kenneth@pigno.se>
2024-09-06 15:14:15 +0900

    feat: add profile code

--- convert_profile.py
+++ convert_profile.py
@@ -13,13 +13,41 @@
 # limitations under the License.
 # ==============================================================================
 
+import linecache
 import os
+import tracemalloc
 from pathlib import Path
 
 import ai_edge_torch
 import torch
 from ai_edge_torch.generative.examples.gemma import gemma2
 from ai_edge_torch.generative.quantize import quant_recipes
+
+
+def display_top(snapshot, key_type='lineno', limit=3):
+    snapshot = snapshot.filter_traces((
+        tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
+        tracemalloc.Filter(False, "<unknown>"),
+    ))
+    top_stats = snapshot.statistics(key_type)
+
+    print("Top %s lines" % limit)
+    for index, stat in enumerate(top_stats[:limit], 1):
+        frame = stat.traceback[0]
+        # replace "/path/to/module/file.py" with "module/file.py"
+        filename = os.sep.join(frame.filename.split(os.sep)[-2:])
+        print("#%s: %s:%s: %.1f KiB"
+              % (index, filename, frame.lineno, stat.size / 1024))
+        line = linecache.getline(frame.filename, frame.lineno).strip()
+        if line:
+            print('    %s' % line)
+
+    other = top_stats[limit:]
+    if other:
+        size = sum(stat.size for stat in other)
+        print("%s other: %.1f KiB" % (len(other), size / 1024))
+    total = sum(stat.size for stat in top_stats)
+    print("Total allocated size: %.1f KiB" % (total / 1024))
 
 
 def convert_gemma_to_tflite(
@@ -61,10 +89,14 @@ def convert_gemma_to_tflite(
         .convert(quant_config=quant_config)
     )
     edge_model.export(output_path)
-
-
+  
+  
 if __name__ == "__main__":
+    snapshot = tracemalloc.take_snapshot()
+    tracemalloc.start()
     project_root = Path(__file__).parent.resolve()
     checkpoint_path = project_root / "model"
     output_path = project_root / "model" / "gemma2.tflite"
     convert_gemma_to_tflite(str(checkpoint_path), str(output_path))
+    display_top(snapshot)
+  
